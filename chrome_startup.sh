d3ea1b8f739ef4e6c4b43389a1c4780b28814496
Karin van den Berg <karin@endouble.com>
2017-07-03 09:23:39 +0200

    Add startup script

---
+++ chrome_startup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Source: https://superuser.com/a/864918
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
